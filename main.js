6e2e36fe2db02077489cc4bf045fb143bbd1495b
pablo <pablo@hotmail.com>
2023-12-04 22:53:43 -0600

    comment app .use.parser

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ const app = express();
 const port = 3000;
 app.use(cors());
 
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.raw());
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
